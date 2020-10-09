59b8dbd9646da2e4ac7d40509de5a1ada5892f9d
Kochiyama <marcelokochiyama2210@gmail.com>
2020-10-09 11:21:05 -0300

    set up for gatsby cloud deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple`, fontFamily: `Raleway` }}>
+      <Header text="Hello Gatbsy!" />
+      <p>What a world.</p>
+      <img
+        src="https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        alt="Gtr"
+      />
+      <Link to="/contact/">Contact</Link>
+    </div>
+  )
 }
